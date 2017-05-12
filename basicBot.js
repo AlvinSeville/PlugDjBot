bf7b522c5e6bfafdda2d5eba32a697c7490d0672
AlvinSeville <tom.jiran@email.cz>
2017-05-12 20:21:37 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -274,7 +274,7 @@
             voteSkipLimit: 10,
             historySkip: false,
             timeGuard: true,
-            maximumSongLength: 10,
+            maximumSongLength: 7,
             autodisable: false,
             commandCooldown: 30,
             usercommandsEnabled: true,
